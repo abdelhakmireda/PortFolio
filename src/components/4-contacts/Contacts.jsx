3650083d029e03f688beea145cb5af854b4a056a
abdelhakmireda <abdelhakmireda37@gmail.com>
2024-03-21 12:55:12 +0000

    refacture:custumos and creation contacts component

--- src/components/4-contacts/Contacts.jsx
+++ src/components/4-contacts/Contacts.jsx
@@ -2,7 +2,29 @@ import './contacts.css'
 
 function Contacts() {
   return (
-    <div>Contacts</div>
+    <section className='contact-us'>
+      <h1 className='title'>
+        <span className='icon-envelope'></span>
+        Contact us 
+      </h1>
+      <p className='subtitle'>
+      Contact us for more information reda abdelhakmi reda oupsi
+      </p>
+      <div className="flex">
+        <form className=''>
+          <div className='flex'>
+            <label htmlFor="email">Email Address:</label>
+            <input type="email" id="email" />
+          </div>
+          <div className='flex'>
+            <label htmlFor="message">Your message:</label>
+          <textarea required name="message" id="message"></textarea>
+          </div>
+          <button className='submit'>Submit</button>
+        </form>
+        <div className=" border animation"></div>
+      </div>
+    </section>
   )
 }
 
