6fd3ff0b47b28c3c4b9b541072d4629a27b22f11
abdelhakmireda <abdelhakmireda37@gmail.com>
2024-03-26 16:06:27 +0000

    refact:customs blur for mobile

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
       <div />
       <nav>
         <ul className='flex'>
-          <li><a href="#hero">About</a></li>
+          <li><a  href="#hero">About</a></li>
           <li><a href="">Articles</a></li>
           <li><a href="">Projects</a></li>
           <li><a href="">Speaking</a></li>
