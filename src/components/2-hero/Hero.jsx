62e99f8e9c7ad80a5782d35c0420414f03ac558e
abdelhakmireda <abdelhakmireda37@gmail.com>
2024-03-27 12:53:03 +0000

    refact:add title and link to icon

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -27,10 +27,10 @@ const Hero = () => {
           
            </p>
         <div className=" all-icons flex">
-          <div className="icon icon-twitter"></div>
-          <div className="icon icon-instagram"></div>
-          <div className="icon icon-github"></div>
-          <div className="icon icon-linkedin"></div>
+          <a href="mailto:abdelhakmireda@gmail.com?subject=Developpeur FullStack(Symfony/React)" className=" icon-envelope" id='icon'> </a>
+          <a href="https://github.com/abdelhakmireda" className=" icon-github" id='icon' title='Télechargé Mon Cv'> </a>
+          <a href="https://www.linkedin.com/in/reda-abdelhakmi-4146371b3/" className=" icon-linkedin" id='icon' title='Linkedin'> </a>
+          <a href="../../../public/abdelhakmireda.pdf" target='_blank' className="icon-cloud-download" id='icon' title='Télechargé Mon Cv'> </a>
         </div>
       </div>
       <div className='right-section animation'>
